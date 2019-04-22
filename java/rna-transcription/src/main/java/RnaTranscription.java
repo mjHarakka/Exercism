8698960d14abbbb0beb209c0b4cf1b4cb56981fb
mjHarakka <mj.harakka@gmail.com>
2019-04-22 23:23:10 +0300

    some modifications

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,37 @@
 class RnaTranscription {
 
-    String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public static void main(String[] args) {
+
     }
 
+
+    String transcribe(String dnaStrand) {
+        char[] charArr = dnaStrand.toCharArray();
+        char[] rnaStrand = new char[charArr.length]; 
+       
+        if (dnaStrand.length() == 0) {
+            return new String("");
+        } else {
+            for (int i = 0; i < charArr.length; i++) {
+                switch (charArr[i]) {
+                    case 'G':
+                        rnaStrand[i] = 'C';
+                        break;
+                    case 'C':
+                        rnaStrand[i] = 'G';
+                        break;
+                    case 'T':
+                        rnaStrand[i] = 'A';
+                        break;
+                    case 'A':
+                        rnaStrand[i] = 'U';
+                        break;
+                }
+            }
+        }
+        return rnaStrand.toString();        
+
+        }
+
+    
 }
