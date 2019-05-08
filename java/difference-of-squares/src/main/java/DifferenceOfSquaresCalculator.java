4d0025cdbbd857d84d1a54c91cdd89bdc8b6e435
mjHarakka <mj.harakka@gmail.com>
2019-05-08 22:05:42 +0300

    improved the solution

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,21 +1,17 @@
 import java.lang.Math;
+import java.util.stream.IntStream;
 
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        int sum = 0;
-        for (int i = 1; i <= input; i++) {
-            sum += i;
-        }
-        return sum * sum;
+        return (int) Math.pow(IntStream.rangeClosed(1, input).sum(), 2);
     }
 
     int computeSumOfSquaresTo(int input) {
-        int sum = 0;
-        for (int i = 1; i <= input; i++) {
-            sum += i * i; 
-        }
-        return sum;
+        return IntStream.rangeClosed(1, input)
+            .parallel()
+            .map(i -> (int) Math.pow(i, 2))
+            .sum();
     }
 
     int computeDifferenceOfSquares(int input) {
